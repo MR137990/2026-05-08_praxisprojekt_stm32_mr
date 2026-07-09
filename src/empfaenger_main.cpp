95cd85e4fa49101e0e705e5fe0a68458f5cb68f4
MR137990 <marvin.richter@study.hs-duesseldorf.de>
2026-07-09 16:55:24 +0200

    ALLES FEDDICH!!

--- src/empfaenger_main.cpp
+++ src/empfaenger_main.cpp
@@ -7,6 +7,10 @@ LR1121 radio = new Module(LR_NSS, LR_IRQ, LR_RESET, LR_BUSY);
 
 LoRaConfig activeConfig;
 
+// --- Globaler Schlüssel für die XOR-Entschlüsselung (Muss identisch zum Sender sein!) ---
+const uint8_t xorKey[] = { 0x5A, 0xA5, 0x1F, 0xE1, 0x3C, 0xC3, 0x7F, 0xF7 };
+const size_t keySize = sizeof(xorKey);
+
 void wait_busy() {
   // Der LR1121 signalisiert "Beschäftigt" durch ein HIGH Signal
   while(digitalRead(LR_BUSY) == HIGH) {
@@ -84,6 +88,59 @@ void loop(){
     int state = radio.readData((uint8_t*)&empfangeneDaten, sizeof(empfangeneDaten));
 
     if(state == RADIOLIB_ERR_NONE) {
+      
+      // =========================================================================
+      // INLINE XOR-ENTSCHLÜSSELUNG (Klartext wiederherstellen)
+      // =========================================================================
+      // Wir betrachten das empfangene Struct als reines Byte-Array
+      uint8_t* bytePointer = (uint8_t*)&empfangeneDaten;
+      size_t datenGroesse = sizeof(empfangeneDaten);
+
+      // Symmetrische Rückrechnung: Ciphertext XOR Key = Klartext
+      for (size_t i = 0; i < datenGroesse; i++) {
+        bytePointer[i] ^= xorKey[i % keySize];
+      }
+      // =========================================================================
+
+      // Ab hier liegen die Daten in 'empfangeneDaten' wieder im originalen Klartext vor!
+      
+      // packet was successfully received
+      Serial.println(F("[LR1121] Received packet!"));
+
+      // print data of the packet
+      Serial.println(F("[LR1121] Data:"));
+      Serial.println("Temperature:");
+      Serial.println(empfangeneDaten.temp);
+      Serial.println("X:");
+      Serial.println(empfangeneDaten.acce_x, 4);
+      Serial.println("Y:");
+      Serial.println(empfangeneDaten.acce_y, 4);
+      Serial.println("Z:");
+      Serial.println(empfangeneDaten.acce_z, 4);
+
+      // print RSSI (Received Signal Strength Indicator)
+      Serial.print(F("[LR1121] RSSI:\t\t"));
+      Serial.print(radio.getRSSI());
+      Serial.println(F(" dBm"));
+
+      // print SNR (Signal-to-Noise Ratio)
+      Serial.print(F("[LR1110] SNR:\t\t"));
+      Serial.print(radio.getSNR());
+      Serial.println(F(" dB"));
+      
+      // WICHTIG: Nach dem Empfang den Chip wieder explizit in den Empfangsmodus versetzen!
+      radio.startReceive();
+    } 
+  }
+}
+
+/*
+void loop(){
+  if(digitalRead(LR_IRQ) == HIGH) {
+    package empfangeneDaten;
+    int state = radio.readData((uint8_t*)&empfangeneDaten, sizeof(empfangeneDaten));
+
+    if(state == RADIOLIB_ERR_NONE) {
       // packet was successfully received
       Serial.println(F("[LR1121] Received packet!"));
 
@@ -110,3 +167,4 @@ void loop(){
     } 
   }
 }
+*/
